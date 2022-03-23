7fd17e0caadc3299d3585c9dc7d8d1fa03dbed75
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-23 15:37:28 -0400

    Began new JavaScript Challenge

---
+++ script.js
@@ -0,0 +1,11 @@
+teamDolphins = 96, 108, 89;
+teamKoalas = 88, 91, 110;
+
+function scoreCalculator(){
+
+    teamDolphins = prompt(Number(`What are the Dolphins scores?`))
+    teamKoalas = prompt(Number(`What are `))
+
+    avarageScore 
+
+}
