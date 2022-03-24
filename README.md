400457c838ccb650c3d9dcf83f3767d85b392dcf
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-24 14:16:54 -0400

    Completed the READ.me and refactored code.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# average_score_calculator
-Comparing scores from two separate teams using if else statements and type conversion's.
+# Average Score Calculator
+Comparing scores from two separate teams using If Else Statements and Type Conversion's.
+
+## Description:
+
+My second coding challenge from, "The Complete JavaScript Course 2022", was to make a calculator that took three different scores from two different teams, averaged the three scores from each team, and determined who had the higher score using If Else Statements. While this again was mainly review for me it did prove more challenging then I initially expected as the logic within the If Else Statements proved to be more complex than I anticipated. Instead of console.logging the results as requested, I made a little project out of it where the user could make two custom named teams and enter different values each calculation.
+
+## What I changed:
+
+I added a button titled "calculate" which provides the user a prompt requesting Team 1's name, Team 2's name, as well as their separate scores so the user can change the values easily.
+
+## What I learned:
+
+This was my first-time using Type Conversion. The scores entered by the user were converted from Strings to Numbers and plugged into an equation to average the scores. The instructor, Jonas, made a bonus challenge for his students by requesting not only make Logical Statements that determine who won with the average greater score, but to make a minimal score of 100 for a team to qualify. This proved more challenging that I expected but with playing with the syntax I was able to get the code to work as I visioned. I ran into bugs were some of the code would work and others wouldn’t, changes would be made and other lines of code wouldn’t work while others did. After some dedication and determination, I wrote the working Logic Statements to have all lines of code work as designed. I reviewed this challenge in December and struggled immensely and to for this challenge to come with the ease that it did was very rewarding. 
+
+![webpage snapshot](/team_score_calculator_screenshot.png)
+
+The Average Score Calculator Challenge 👉🏽 https://coderortiz.github.io/average_score_calculator/
+
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function scoreCalculator() {
 
     // Rules for the Calculation
-    alert(`The average rounded score will be calculated but both teams need to at least reach a minimal rounded score of 100. For there to be a draw, both teams must get at least a rounded score of 100 or more.`)
+    alert(`The average rounded score will be calculated but both teams need to at least reach a minimal rounded score of 100. For there to be a draw, both teams must get at least a rounded score of 100 or more. The team with the higher rounded average that is 100 or more wins!`)
 
     // Naming Team 1
     let team1Name = prompt("What is the first teams name?");
@@ -19,7 +19,7 @@ function scoreCalculator() {
     let team2b = prompt(`What is the ${team2Name} second score?`);
     let team2c = prompt(`What is the ${team2Name} third score?`);
 
-    // Calculating Average Score of Both Teams.
+    // Calculating Average Score of Both Teams using Type Conversion.
     let team1Avg = (Number(team1a) + Number(team1b) + Number(team1c)) / 3;
     let team2Avg = (Number(team2a) + Number(team2b) + Number(team2c)) / 3;
 
@@ -33,36 +33,30 @@ function scoreCalculator() {
         // Both Teams have a Higher Score of 100 or More and Drawed
         if (((team1RoundedAvg >= 100) === (team2RoundedAvg >= 100)) && (team1RoundedAvg === team2RoundedAvg)) {
         document.querySelector("h3").textContent = `Looks like both teams drawed and had a rounded average of ${team1RoundedAvg}.`;
-        console.log(team1RoundedAvg, team2RoundedAvg)
         }
 
         // Team 1 had the Higher Score and Both Teams had a score of 100 or More
         else if (((team1RoundedAvg >= 100) && (team2RoundedAvg >= 100)) && (team1RoundedAvg > team2RoundedAvg)) {
         document.querySelector("h3").textContent = `${team1Name} won with an average rounded score of, ${team1RoundedAvg}. ${team2Name} lost and had an average rounded score of, ${team2RoundedAvg}.`;
-        console.log(team1RoundedAvg, team2RoundedAvg)
         }
 
         // Team 1 had the Higher Score and Qualified, while Team 2 Did Not Qualify
         else if (((team1RoundedAvg >= 100) && (team2RoundedAvg < 100)) && (team1RoundedAvg > team2RoundedAvg)) {
-        document.querySelector("h3").textContent = `${team1Name} won with an average rounded score of, ${team1RoundedAvg}. ${team2Name} was disqualified with an average rounded score of, ${team2RoundedAvg}.`
-        console.log(team1RoundedAvg, team2RoundedAvg)
+        document.querySelector("h3").textContent = `${team1Name} won with an average rounded score of, ${team1RoundedAvg}. ${team2Name} was disqualified with an average rounded score of, ${team2RoundedAvg}.`;
         }
 
         // Team 2 had the Higher Score and Both Teams had a score of 100 or More
         else if (((team1RoundedAvg >= 100) && (team2RoundedAvg >= 100)) && (team1RoundedAvg < team2RoundedAvg)) {
-        document.querySelector("h3").textContent = `${team2Name} won with an average rounded score of, ${team2RoundedAvg}, while ${team1Name} lost with an average rounded score of, ${team1RoundedAvg}.`
-        console.log(team1RoundedAvg, team2RoundedAvg)
+        document.querySelector("h3").textContent = `${team2Name} won with an average rounded score of, ${team2RoundedAvg}, while ${team1Name} lost with an average rounded score of, ${team1RoundedAvg}.`;
          }
 
         // Team 2 had the Higer Score and Qualified, while Team 1 Did Not Qualify
         else if (((team1RoundedAvg < 100) && (team2RoundedAvg >= 100)) && (team1RoundedAvg < team2RoundedAvg)) {
-        document.querySelector("h3").textContent = `${team2Name} won with an average rounded score of, ${team2RoundedAvg}. ${team1Name} was disquaified with an average rounded score of, ${team1RoundedAvg}.`
-        console.log(team1RoundedAvg, team2RoundedAvg)
+        document.querySelector("h3").textContent = `${team2Name} won with an average rounded score of, ${team2RoundedAvg}. ${team1Name} was disquaified with an average rounded score of, ${team1RoundedAvg}.`;
         }
 
         // Both Teams were Disqualified
         else {
-        document.querySelector("h3").textContent = `Both teams were disquaified. ${team1Name} had an average rounded score of, ${team1RoundedAvg}, and ${team2Name} had an average rounded score of, ${team2RoundedAvg}.`
-        console.log(team1RoundedAvg, team2RoundedAvg)
+        document.querySelector("h3").textContent = `Both teams were disquaified. ${team1Name} had an average rounded score of, ${team1RoundedAvg}, and ${team2Name} had an average rounded score of, ${team2RoundedAvg}.`;
         }
 } 
