dce3b4c7def669936c8bf838f01e5e6bd14fa547
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-03-24 12:48:43 -0400

    Added to the Logic. Both teams as well  must have a higher score of 99 to qualify for winning.

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
-function scoreCalculator(){
+function scoreCalculator() {
+
+    // Rules for the Calculation
+    alert(`The average rounded score will be calculated but both teams need to at least reach a minimal rounded score of 100. For there to be a draw, both teams must get at least a rounded score of 100 or more.`)
 
     // Naming Team 1
-    team1Name = prompt("What is the first teams name?");
+    let team1Name = prompt("What is the first teams name?");
 
     // Collecting Team 1's Three Scores
-    team1a = prompt(`What is the ${team1Name} first score?`);
-    team1b = prompt(`What is the ${team1Name} second score?`);
-    team1c = prompt(`What is the ${team1Name} third score?`);
+    let team1a = prompt(`What is the ${team1Name} first score?`);
+    let team1b = prompt(`What is the ${team1Name} second score?`);
+    let team1c = prompt(`What is the ${team1Name} third score?`);
 
     // Naming Team 2
-    team2Name = prompt("What is the second team's name?");
+    let team2Name = prompt("What is the second team's name?");
 
     // Collecting Team 2's Three Scores
-    team2a = prompt(`What is the ${team2Name} first score?`);
-    team2b = prompt(`What is the ${team2Name} second score?`);
-    team2c = prompt(`What is the ${team2Name} third score?`);
-    
+    let team2a = prompt(`What is the ${team2Name} first score?`);
+    let team2b = prompt(`What is the ${team2Name} second score?`);
+    let team2c = prompt(`What is the ${team2Name} third score?`);
+
     // Calculating Average Score of Both Teams.
-    team1Avg = (Number(team1a) + Number(team1b) + Number(team1c)) / 3;
-    team2Avg = (Number(team2a) + Number(team2b) + Number(team2c)) / 3;
+    let team1Avg = (Number(team1a) + Number(team1b) + Number(team1c)) / 3;
+    let team2Avg = (Number(team2a) + Number(team2b) + Number(team2c)) / 3;
 
     // Rounded Both Teams Average Scores
-    team1RoundedAvg = Math.round(team1Avg);
-    team2RoundedAvg = Math.round(team2Avg);
+    let team1RoundedAvg = Math.round(team1Avg);
+    let team2RoundedAvg = Math.round(team2Avg);
+
 
     // Changing the Text Content of the h3 to Display the Winner and the Average Scores
-    if (team1RoundedAvg === team2RoundedAvg){
-        document.querySelector("h3").textContent = `Looks like both teams averaged ${team1RoundedAvg} and drawed.`;
-    } 
-    else if (team1RoundedAvg > team2RoundedAvg){
-        document.querySelector("h3").textContent = `${team1Name} won with an average score of, ${team1RoundedAvg}. ${team2Name} had an average score of ${team2RoundedAvg}.`;
-    }
-    else {
-        document.querySelector("h3").textContent = `${team2Name} won with an average score of, ${team2RoundedAvg}. ${team1Name} had an average score of ${team1RoundedAvg}.`;
-    }
-    console.log(team1RoundedAvg, team2RoundedAvg)
-}
+
+        // Both Teams have a Higher Score of 100 or More and Drawed
+        if (((team1RoundedAvg >= 100) === (team2RoundedAvg >= 100)) && (team1RoundedAvg === team2RoundedAvg)) {
+        document.querySelector("h3").textContent = `Looks like both teams drawed and had a rounded average of ${team1RoundedAvg}.`;
+        console.log(team1RoundedAvg, team2RoundedAvg)
+        }
+
+        // Team 1 had the Higher Score and Both Teams had a score of 100 or More
+        else if (((team1RoundedAvg >= 100) && (team2RoundedAvg >= 100)) && (team1RoundedAvg > team2RoundedAvg)) {
+        document.querySelector("h3").textContent = `${team1Name} won with an average rounded score of, ${team1RoundedAvg}. ${team2Name} lost and had an average rounded score of, ${team2RoundedAvg}.`;
+        console.log(team1RoundedAvg, team2RoundedAvg)
+        }
+
+        // Team 1 had the Higher Score and Qualified, while Team 2 Did Not Qualify
+        else if (((team1RoundedAvg >= 100) && (team2RoundedAvg < 100)) && (team1RoundedAvg > team2RoundedAvg)) {
+        document.querySelector("h3").textContent = `${team1Name} won with an average rounded score of, ${team1RoundedAvg}. ${team2Name} was disqualified with an average rounded score of, ${team2RoundedAvg}.`
+        console.log(team1RoundedAvg, team2RoundedAvg)
+        }
+
+        // Team 2 had the Higher Score and Both Teams had a score of 100 or More
+        else if (((team1RoundedAvg >= 100) && (team2RoundedAvg >= 100)) && (team1RoundedAvg < team2RoundedAvg)) {
+        document.querySelector("h3").textContent = `${team2Name} won with an average rounded score of, ${team2RoundedAvg}, while ${team1Name} lost with an average rounded score of, ${team1RoundedAvg}.`
+        console.log(team1RoundedAvg, team2RoundedAvg)
+         }
+
+        // Team 2 had the Higer Score and Qualified, while Team 1 Did Not Qualify
+        else if (((team1RoundedAvg < 100) && (team2RoundedAvg >= 100)) && (team1RoundedAvg < team2RoundedAvg)) {
+        document.querySelector("h3").textContent = `${team2Name} won with an average rounded score of, ${team2RoundedAvg}. ${team1Name} was disquaified with an average rounded score of, ${team1RoundedAvg}.`
+        console.log(team1RoundedAvg, team2RoundedAvg)
+        }
+
+        // Both Teams were Disqualified
+        else {
+        document.querySelector("h3").textContent = `Both teams were disquaified. ${team1Name} had an average rounded score of, ${team1RoundedAvg}, and ${team2Name} had an average rounded score of, ${team2RoundedAvg}.`
+        console.log(team1RoundedAvg, team2RoundedAvg)
+        }
+} 
